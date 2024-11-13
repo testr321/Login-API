4cf00a030eaf0d23ee11918226271ca5edc9f7ce
Bryan <bryan.bong@itchitech.com>
2024-11-13 18:53:40 +0800

    fdsa

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+app.use(cors())
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false, parameterLimit:50000 }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
