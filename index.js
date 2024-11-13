58b481d2a70113e276c2650304192aa8291dce75
Bryan <bryan.bong@itchitech.com>
2024-11-13 19:14:04 +0800

    add login and register API

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use(cors())
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false, parameterLimit:50000 }));
 
+const config = {
+  connectionString:
+    "postgres://testing_database_vh02_user:8VlFnQqxZLbyde2vVc8mAYFQCyMh8CXy@dpg-csq8d8bqf0us73ed63f0-a.singapore-postgres.render.com/testing_database_vh02?ssl=true"
+};
+
+const { Client } = require('pg');
+const { constants } = require("buffer");
+const client = new Client(config);
+client.connect();
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -28,12 +38,31 @@ app.post('/login', (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
   
-  var data = {
-    username: username,
-    password: password,
-  }
+  client.query("SELECT * FROM users WHERE username = $1 AND password = $2", [username, password])
+  .then((result) => {
+    if (result.rows.length > 0)
+    {
+      return res.status(200).send("Welcome, " + result.rows[0].username);
+    }
+    else
+    {
+      return res.status(200).send("Wrong username/password");
+    }
+    
+  });
+})
+
+app.post('/register', (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
   
-  return res.json(data);
+  client.query("INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *", [username, password])
+  .then((result) => {
+    // result.rows[0].
+    return res.status(200).send("Success");
+  });
+
+  
 })
 
 app.listen(port, () => {
